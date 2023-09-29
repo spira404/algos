7613e58ac6c4c2267fac9e0d27e55ea48b7705ad
spira404 <spiiira404@gmail.com>
2023-09-29 18:38:25 +0300

    bellmanford

--- src/bellmanFord.go
+++ src/bellmanFord.go
@@ -4,39 +4,74 @@ import(
 	"fmt"
 )
 
-// using this insted of math.Inf bc it"s int
+// using this insted of math.Inf bc it's int
 var inf = 10000000
-var processed = map[string]int{}
 
-func bellmanford(){
-	pass
+func bellmanford(graph [][]int, src int){
+	
+	V := len(graph)
+	dist := make([]int, V-1)
+
+	for i := range dist{
+		dist[i] = inf
+	}
+
+	dist[src] = 0
+	for i := 0; i < V-1; i++{
+		for _, j := range graph{
+			u, v, w := j[0], j[1], j[2] 
+			if dist[u] != inf && dist[u] + w < dist[v]{
+				dist[v] = dist[u] + w
+			}
+		}
+	}
+
+	for _, j := range graph{
+		u, v, w := j[0], j[1], j[2] 
+		if dist[u] != inf && dist[u] + w < dist[v]{
+			fmt.Println("At least one negative cycle exist!")
+			return
+		}
+	}
+
+	for idx, weight := range dist{
+		fmt.Printf("From source to vertex %d - %d \n", idx, weight)}
 }
 
 
 func main(){
-	// init test graph by hands bc it's about algo
-	// not about methods of creating graphs
-      // 6      ┌───┐     1
-  // ┌─────────►│ a ├───────────┐
-  // │          └───┘           ▼
-// ┌─┴──┐         ▲           ┌───┐
-// │strt│         │           │end│
-// └─┬──┘         │           └───┘
-  // │          ┌─┴─┐           ▲
-  // └─────────►│ b ├───────────┘
-      // 2      └───┘     5
-	
-	graph := map[string]map[string]int{"strt": {"a": 6, "b": 2}, 
-	"a": {"end": 1},
-	"b": {"a": 3, "end": 5},
-	"end": {}}
+//	            ┌───┐          ┌───┐
+//	  ┌───5────►│ 1 ├────2────►│ 3 ├────2────┐
+//	  │         └─┬─┘          └───┘         │
+//	  │           │              ▲           ▼
+//	┌─┴─┐         1              │         ┌───┐
+//	│ 0 │         │             -1         │ 5 │
+//	└───┘         ▼              │         └─┬─┘
+//	            ┌───┐          ┌─┴─┐         │
+//	            │ 2 ├────1────►│ 4 │◄───-3───┘
+//	            └───┘          └───┘
+	graph := [][]int{
+		{0, 1, 5},
+		{1, 2, 1},
+		{1, 3, 2},
+		{2, 4, 1},
+		{3, 5, 2},
+		{4, 3, -1},
+		{5, 4, -3}}
 
-	parents := map[string]string{"a": "strt", 
-	"b": "strt",
-	"end": ""}
+	bellmanford(graph, 0)
 
-	costs := map[string]int{"a": 6, 
-	"b": 2, 
-	"end": inf}
+	// change the last weight to +
+	graph = [][]int{
+		{0, 1, 5},
+		{1, 2, 1},
+		{1, 3, 2},
+		{2, 4, 1},
+		{3, 5, 2},
+		{4, 3, -1},
+		{5, 4, 3}}
 
+	bellmanford(graph, 0)
 	}
+
+
